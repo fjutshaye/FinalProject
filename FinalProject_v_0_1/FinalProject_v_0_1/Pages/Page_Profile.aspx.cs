6152f52a07c4f1e59709c0eb001d7a82f7871577
fjutshaye <fjutshaye@gmail.com>
2016-11-25 09:44:30 -0500

    2016-11-25

--- FinalProject_v_0_1/FinalProject_v_0_1/Pages/Page_Profile.aspx.cs
+++ FinalProject_v_0_1/FinalProject_v_0_1/Pages/Page_Profile.aspx.cs
@@ -9,13 +9,13 @@ namespace FinalProject_v_0_1.Pages
         private object[] userProfile;
         protected void Page_Load(object sender, EventArgs e)
         {
-            //username = (string)Session["username"];
-            //userProfile = DatabaseTools.queryProfile(username);
+            username = (string)Session["username"];
+            userProfile = DatabaseTools.queryProfile(username);
 
-            //TextBox_Name.Text = userProfile[1].ToString();
-            //TextBox_Phone.Text = userProfile[2].ToString();
-            //TextBox_Email.Text  = userProfile[0].ToString();
-            //TextBox_CarInfo.Text  = userProfile[4].ToString();
+            TextBox_Name.Text = userProfile[1].ToString();
+            TextBox_Phone.Text = userProfile[2].ToString();
+            TextBox_Email.Text = userProfile[0].ToString();
+            TextBox_CarInfo.Text = userProfile[4].ToString();
         }
     }
 }
